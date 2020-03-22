0b01adf50bee3aa2dd2d2111cc3ee3c8f05a0ea8
quanee <262504455@qq.com>
2020-03-22 18:16:53 +0800

    search logic

---
+++ logic/search.go
@@ -0,0 +1,46 @@
+package logic
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/quanee/qlog/model"
+	"github.com/quanee/qlog/utils/log"
+	"time"
+)
+
+//	SearchArticle: search by words
+func SearchArticle(db *sql.DB, words string) ([]model.Summary, error) {
+	ql := fmt.Sprintf("SELECT id, title, abstract, created_time FROM summary WHERE id in (SELECT id FROM content WHERE MATCH(substance) AGAINST('%s' IN BOOLEAN MODE));", words)
+	rows, err := db.Query(ql)
+	defer func() {
+		if rows != nil {
+			// 可以关闭掉未scan连接一直占用
+			rows.Close()
+		}
+	}()
+	if err != nil {
+		log.Error("query all err ", err)
+		return []model.Summary{}, err
+	}
+	var res []model.Summary
+	var summary model.Summary
+	for rows.Next() {
+		err = rows.Scan(&summary.SId, &summary.Title, &summary.Abstract, &summary.CreatedTime)
+		if err != nil {
+			log.Errorf("search Article error", err)
+		}
+		timeParse, terr := time.Parse("2006-01-02 15:04:05", summary.CreatedTime)
+		if terr != nil {
+			log.Error("Parser time error ")
+		}
+		summary.CreatedTime = fmt.Sprint(timeParse.Format("02 Jan 06"))
+		res = append(res, summary)
+	}
+	timeParse, terr := time.Parse("2006-01-02 15:04:05", summary.CreatedTime)
+	if terr != nil {
+		log.Error("Parser time error ")
+	}
+	summary.CreatedTime = fmt.Sprint(timeParse.Format("02 Jan 06"))
+	log.Print("Search article by words:", words)
+	return res, nil
+}
