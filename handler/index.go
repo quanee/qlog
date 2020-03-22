9dac737a84389f980d7501e5bbaa25f744d24c8d
quanee <262504455@qq.com>
2020-03-22 18:05:17 +0800

    handler index

---
+++ handler/index.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"github.com/quanee/draft"
+	"github.com/quanee/qlog/database"
+	"github.com/quanee/qlog/logic"
+	"github.com/quanee/qlog/utils/http2"
+	"github.com/quanee/qlog/utils/log"
+	"net/http"
+	"os"
+)
+
+func Index(context *draft.Context) {
+	http2.Push(context.Writer, "/static/css/quanee.css", "/static/js/quanee.js", "/static/favicon.webp")
+
+	articles, err := logic.QueryLimitSummary(database.DB, "0", "10")
+	if err != nil {
+		log.Errorf("QueryLimitSummary", err)
+	}
+
+	context.HTML(http.StatusOK, "index.tpl", draft.H{
+		"title":       "首页",
+		"articles":    articles,
+		"host":        "http://www.quanee.com/",
+		"description": "首页",
+		"slogan":      "Feature oriented programming",
+		"env":         os.Getenv("ENV"),
+	})
+}
