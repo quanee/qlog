46df2f346d93533b41f8a9c8b23a3cba981f7ae0
quanee <262504455@qq.com>
2020-03-22 18:18:23 +0800

    log middleware

---
+++ middleware/logger.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"github.com/quanee/draft"
+	"github.com/quanee/qlog/utils/log"
+	"time"
+)
+
+func Logger() draft.HandlerFunc {
+	return func(c *draft.Context) {
+		// 开始时间
+		t := time.Now()
+		// 处理请求
+		c.Next()
+		// 计算处理时间
+		log.Infof("[%s] %v in %v", c.Req.RequestURI, c.StatusCode, time.Since(t))
+	}
+}
