bd01c342b78774423234d872f762e896c0b4a921
quanee <262504455@qq.com>
2020-03-22 18:05:51 +0800

    handler search

---
+++ handler/search.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"github.com/quanee/draft"
+	"github.com/quanee/qlog/database"
+	"github.com/quanee/qlog/logic"
+	"github.com/quanee/qlog/utils/log"
+	"net/http"
+	"os"
+)
+
+func Search(context *draft.Context) {
+	words := context.QueryParam("param")
+	articles, err := logic.SearchArticle(database.DB, words)
+	if err != nil {
+		log.Errorf("Search error", err)
+	}
+
+	if context.Method == "POST" {
+		context.HTML(http.StatusOK, "article.tpl", draft.H{
+			"title":       "搜索",
+			"articles":    articles,
+			"host":        "http://www.quanee.com/",
+			"description": "搜索",
+			"slogan":      "Feature oriented programming",
+			"env":         os.Getenv("ENV"),
+		})
+	} else {
+		context.HTML(http.StatusOK, "search.tpl", draft.H{
+			"title":       "搜索",
+			"articles":    articles,
+			"host":        "http://www.quanee.com/",
+			"description": "搜索",
+			"slogan":      "Feature oriented programming",
+			"env":         os.Getenv("ENV"),
+		})
+	}
+
+}
