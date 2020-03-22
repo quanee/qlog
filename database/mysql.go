4ba5aa34ae553aab504decf300cf3dc76d21df95
quanee <262504455@qq.com>
2020-03-22 18:04:10 +0800

    mysql connect config

---
+++ database/mysql.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/quanee/qlog/config"
+	"github.com/quanee/qlog/utils/log"
+	"time"
+)
+
+func init() {
+	if config.GetKey("use_database") == "mysql" {
+		once.Do(func() {
+			dsn := fmt.Sprintf("%s:%s@%s(%s:%s)/%s",
+				config.GetKey("mysql_user"),
+				config.GetKey("mysql_passwd"),
+				"tcp",
+				config.GetKey("mysql_host"),
+				config.GetKey("mysql_port"),
+				config.GetKey("mysql_database"),
+			)
+
+			var err error
+			DB, err = sql.Open("mysql", dsn)
+			if err != nil {
+				log.Error("connect err ", err)
+				return
+			}
+			err = DB.Ping()
+			if err != nil {
+				log.Error("ping err ", err)
+				return
+			}
+
+			DB.SetMaxOpenConns(8)
+			DB.SetMaxIdleConns(4)
+			DB.SetConnMaxLifetime(60 * time.Second)
+		})
+	}
+}
