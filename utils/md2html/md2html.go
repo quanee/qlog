d2b54395d536c0f3343d536b42fce2e211ddae2a
quanee <262504455@qq.com>
2020-04-04 12:01:28 +0800

    update

--- utils/md2html/md2html.go
+++ utils/md2html/md2html.go
@@ -1,21 +1,20 @@
 package md2html
 
-import "github.com/88250/lute"
+import "gitlab.com/golang-commonmark/markdown"
 
-func MD2HTML(markdown []byte) (string, error) {
-	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
-	html := luteEngine.Markdown("big", markdown)
-	return string(html), nil
-}
-
-//
-//func MD2HTML(markdowns []byte) (string, error) {
-//	md := markdown.New(markdown.XHTMLOutput(true))
-//	//for _, tok := range md.Parse(markdowns) {
-//	//	fmt.Println(tok.Tag())
-//	//}
-//	return md.RenderToString(markdowns), nil
+//func MD2HTML(markdown []byte) (string, error) {
+//	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
+//	html := luteEngine.Markdown("big", markdown)
+//	return string(html), nil
 //}
+
+func MD2HTML(markdowns []byte) (string, error) {
+	md := markdown.New(markdown.XHTMLOutput(true))
+	//for _, tok := range md.Parse(markdowns) {
+	//	fmt.Println(tok.Tag())
+	//}
+	return md.RenderToString(markdowns), nil
+}
 
 //func MD2HTML(markdown []byte) (string, error) {
 //	output := blackfriday.Run(markdown)
