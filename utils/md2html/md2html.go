cf3d5f21923c4405b8334e4b9a185fd905baec71
quanee <262504455@qq.com>
2020-04-04 11:50:37 +0800

    update

--- utils/md2html/md2html.go
+++ utils/md2html/md2html.go
@@ -1,24 +1,24 @@
 package md2html
 
 import (
-	"github.com/88250/lute"
-	"gitlab.com/golang-commonmark/markdown"
+	"github.com/russross/blackfriday"
 )
 
-func MD2HTMLlute(markdown []byte) (string, error) {
-	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
-	html := luteEngine.Markdown("demo", markdown)
-	return string(html), nil
-}
-
-func MD2HTML(markdowns []byte) (string, error) {
-	md := markdown.New(markdown.XHTMLOutput(true))
-	//for _, tok := range md.Parse(markdowns) {
-	//	fmt.Println(tok.Tag())
-	//}
-	return md.RenderToString(markdowns), nil
-}
-
-//func MD2HTMLbl(markdown []byte) (string, error) {
-//	return string(github_flavored_markdown.Markdown(markdown)), nil
+//func MD2HTMLlute(markdown []byte) (string, error) {
+//	luteEngine := lute.New() // 默认已经启用 GFM 支持以及中文语境优化
+//	html := luteEngine.Markdown("big", markdown)
+//	return string(html), nil
 //}
+//
+//func MD2HTML(markdowns []byte) (string, error) {
+//	md := markdown.New(markdown.XHTMLOutput(true))
+//	//for _, tok := range md.Parse(markdowns) {
+//	//	fmt.Println(tok.Tag())
+//	//}
+//	return md.RenderToString(markdowns), nil
+//}
+
+func MD2HTMLbl(markdown []byte) (string, error) {
+	output := blackfriday.Run(markdown)
+	return string(output), nil
+}
