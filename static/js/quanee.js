2bc65f7a3ef425b5ba3e831b669618682d718e75
quanee <262504455@qq.com>
2020-06-09 09:40:05 +0800

    update

--- static/js/quanee.js
+++ static/js/quanee.js
@@ -3,6 +3,7 @@ var offs = 5;
 var codemirror;
 var md;
 var isGet = true;
+var toggleNavMenu = true;
 
 $(document).ready(function () {
     $('.waves').mousedown(function (e) {
@@ -69,7 +70,7 @@ $('#nav-menu').on('click', function () {
 });
 
 
-function commheadershadow() {
+/*function commheadershadow() {
     //
     $(document).ready(function () {
         $(window).scroll(function () {
@@ -78,14 +79,14 @@ function commheadershadow() {
             } else {
                 $('#nav-header').removeClass('has-shadow')
             }
-            /*if ($(document).scrollTop() >= $(document).height() - $(window).height()) {
+            /!*if ($(document).scrollTop() >= $(document).height() - $(window).height()) {
                 alert('滚动条已经到达底部为' + $(document).scrollTop());
-            }*/
+            }*!/
         }); //
     });
-}
+}*/
 
-if (window.location.pathname == '/edit') {
+/*if (window.location.pathname == '/edit') {
     codemirror = CodeMirror.fromTextArea(document.getElementById('edit'), {
         value: '# hello',
         mode: 'markdown',
@@ -99,7 +100,7 @@ if (window.location.pathname == '/edit') {
         // keyMap: 'sublime',
         // extraKeys: {'Enter': 'newlineAndIndentContinueMarkdownList'}
     });
-}
+}*/
 
 $('#search-btn').click(function () {
     fetch("/search/" + $('#search-input').val(), {
@@ -117,7 +118,7 @@ $('#search-btn').click(function () {
     );
 });
 
-$('#preview-btn').click(
+/*$('#preview-btn').click(
     function () {
         var today = new Date();
         //获取当前年
@@ -146,9 +147,9 @@ $('#preview-btn').click(
         console.log(orderDate);
         $('#preview-title').html($('#input-title').val());
     }
-);
+);*/
 
-$('#edit-btn').click(
+/*$('#edit-btn').click(
     function () {
         md = codemirror.getValue();
         marked.setOptions({
@@ -161,9 +162,9 @@ $('#edit-btn').click(
         $('#input-title').innerText = $('#input-title').val();
         $('#md-edit').innerText = md;
     }
-);
+);*/
 
-$('#post-btn').click(
+/*$('#post-btn').click(
     function () {
         var article = {
             title: $('#input-title').val(),
@@ -182,7 +183,7 @@ $('#post-btn').click(
             window.location.href = '/'
         );
     }
-);
+);*/
 
 function getartic(cid, offset) {
     isGet = false;
@@ -232,12 +233,12 @@ $(window).scroll(function () {
         if ((scrolls >= windowTop) && (scrolls > 110)) {
             //需要执行隐藏导航的操作
             $('#post-nav-title').addClass('nav-title-tran');
-            $('#post-nav-menu').addClass('nav-menu-tran');
+            $('#ln-menu-tray').addClass('nav-menu-tran');
             windowTop = scrolls;
         } else {
             // 上滑
             $('#post-nav-title').removeClass('nav-title-tran');
-            $('#post-nav-menu').removeClass('nav-menu-tran');
+            $('#ln-menu-tray').removeClass('nav-menu-tran');
             windowTop = scrolls;
         }
     }
@@ -259,5 +260,30 @@ function detailpost(s) {
     )
 }
 
-commheadershadow();
+// commheadershadow();
 backTop();
+
+function menuToggle() {
+    // if (document.getElementById("main-navigation").style.display === "none") {
+    // } else {
+    // }
+    if (document.getElementById("nav-icon").style.display !== "none") {
+        document.getElementById("main-navigation").style.display = "none"
+        document.getElementById("nav-icon").style.display = "none"
+        document.getElementById("nav-icon-close").style.display = "block"
+    } else {
+        // $("#main-navigation").toggle();
+        document.getElementById("main-navigation").style.display = "block"
+        document.getElementById("nav-icon").style.display = "block"
+        document.getElementById("nav-icon-close").style.display = "none"
+    }
+    // $("#nav-icon").toggle();
+    // $("#nav-icon-close").toggle();
+    // console.log(window.getComputedStyle(document.getElementById("main-navigation"), "display").content)
+    // if (window.getComputedStyle(document.getElementById("main-navigation"), "display").content != "normal") {
+    //     document.getElementById("main-navigation").style.display = "none"
+    // } else {
+    //     document.getElementById("main-navigation").style.display = "normal"
+    // }
+    // console.log(window.getComputedStyle(document.getElementById("main-navigation"), "display").content)
+}
