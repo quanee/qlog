dc5981529ae426d76b4f41e3dbf29fe57d9f67aa
quanee <262504455@qq.com>
2020-06-09 09:39:24 +0800

    update

--- middleware/cookie.go
+++ middleware/cookie.go
@@ -8,7 +8,8 @@ import (
 func Cookie() draft.HandlerFunc {
 	return func(c *draft.Context) {
 		log.Infof("%v %v %v", c.Req.URL, c.Req.Host, c.Req.Cookies(), c.Req.Method)
-
+		c.SetHeader("Set-Cookie", "Secure; HttpOnly")
+		c.SetHeader("x-xss-protection", "1; mode=block")
 		c.Next()
 	}
 }
